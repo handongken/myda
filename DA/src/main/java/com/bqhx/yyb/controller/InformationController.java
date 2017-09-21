885eadc7a6be90c5dba11478a39419e8a6e053df
yuki9467 <1044081923@qq.com>
2017-09-21 10:31:39 +0800

    update

--- DA/src/main/java/com/bqhx/yyb/controller/InformationController.java
+++ DA/src/main/java/com/bqhx/yyb/controller/InformationController.java
@@ -11,10 +11,8 @@ import org.springframework.web.servlet.ModelAndView;
 import com.bqhx.yyb.dao.InformationVOMapper;
 import com.bqhx.yyb.vo.InformationVO;
 
-
 /**
- * @author Administrator
- * InformationController
+ * @author Administrator InformationController
  */
 
 @RestController
@@ -24,89 +22,85 @@ public class InformationController {
 	@Autowired
 	private InformationVOMapper informationVOMapper;
 	private int flag = 1;
-	
+
 	@RequestMapping(value = "/deleteByPrimaryKey", method = RequestMethod.POST)
-	int deleteByPrimaryKey(String contract){
-		informationVOMapper.deleteByPrimaryKey(contract);		
+	int deleteByPrimaryKey(String contract) {
+		informationVOMapper.deleteByPrimaryKey(contract);
 		return flag;
 	}
-	
+
 	@RequestMapping(value = "/insert", method = RequestMethod.POST)
-	int insert(InformationVO record){
+	int insert(InformationVO record) {
 		informationVOMapper.insert(record);
 		return flag;
 	}
-	
+
 	@RequestMapping(value = "/insertSelective", method = RequestMethod.POST)
-	int insertSelective(InformationVO record){
+	int insertSelective(InformationVO record) {
 		informationVOMapper.insertSelective(record);
 		return flag;
 	}
-	
+
 	@RequestMapping(value = "/selectByPrimaryKey", method = RequestMethod.GET)
-	InformationVO selectByPrimaryKey(String contract){
+	InformationVO selectByPrimaryKey(String contract) {
 		InformationVO informationVO = informationVOMapper.selectByPrimaryKey(contract);
 		return informationVO;
 	}
-	
+
 	@RequestMapping(value = "/updateByPrimaryKeySelective", method = RequestMethod.POST)
-	int updateByPrimaryKeySelective(InformationVO record){
+	int updateByPrimaryKeySelective(InformationVO record) {
 		informationVOMapper.updateByPrimaryKeySelective(record);
 		return flag;
 	}
-	
+
 	@RequestMapping(value = "/updateByPrimaryKey", method = RequestMethod.POST)
-	int updateByPrimaryKey(InformationVO record){
+	int updateByPrimaryKey(InformationVO record) {
 		informationVOMapper.updateByPrimaryKey(record);
 		return flag;
 	}
-	
+
 	/**
-	 * @author Administrator
-	 * 获取绩效
+	 * @author Administrator 获取绩效
 	 */
 	@RequestMapping(value = "/getPerformance", method = RequestMethod.GET)
-	InformationVO getPerformance(String contract){
+	InformationVO getPerformance(String contract) {
 		InformationVO information = informationVOMapper.getPerformance(contract);
-//		int money = information.getMoney();
-		
+		// int money = information.getMoney();
+
 		return information;
 	}
-	
+
 	/**
-	 * @author Administrator
-	 * 更新利息      
+	 * @author Administrator 更新利息
 	 */
 	@RequestMapping(value = "/getInterest", method = RequestMethod.POST)
-	InformationVO updateInterest(String contract){
+	InformationVO updateInterest(String contract) {
 		InformationVO information_before = informationVOMapper.getInterest(contract);
 		int periods = information_before.getPeriods();
 		BigDecimal rate = information_before.getRate();
 		BigDecimal interest_all = rate.multiply(new BigDecimal(periods));
 		BigDecimal interest_month = information_before.getInterestMonth();
-		if(periods != 0){		
+		if (periods != 0) {
 			interest_month = interest_all.divide(new BigDecimal(periods));
-}
-		informationVOMapper.updateInterest(contract, interest_all,interest_month);
+		}
+		informationVOMapper.updateInterest(contract, interest_all, interest_month);
 		InformationVO information_after = informationVOMapper.getInterest(contract);
 		return information_after;
 	}
-	
+
 	/**
-	 * @author Administrator
-	 * 获取利息
+	 * @author Administrator 获取利息
 	 */
-	InformationVO getTnterest(String contract){
+	InformationVO getTnterest(String contract) {
 		InformationVO information = informationVOMapper.getInterest(contract);
 		return information;
 	}
-	
+
 	/**
-	 * @author Administrator
-	 * 获取银行信息
+	 * @author Administrator 获取银行信息
 	 */
 	@RequestMapping(value = "/getBank", method = RequestMethod.GET)
-	InformationVO getBank(String contract){
+	InformationVO getBank(String contract) {
 		InformationVO information = informationVOMapper.getBank(contract);
 		return information;
 	}
