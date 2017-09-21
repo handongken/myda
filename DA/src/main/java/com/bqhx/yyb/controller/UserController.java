c80d4c6a20cc7fcd24d0f0bbfb35e4f9b1bdcbf2
hdken0625 <hdken@163.com>
2017-09-21 10:35:46 +0800

    11

--- DA/src/main/java/com/bqhx/yyb/controller/UserController.java
+++ DA/src/main/java/com/bqhx/yyb/controller/UserController.java
@@ -4,14 +4,17 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bqhx.yyb.dao.UserMapper;
 import com.bqhx.yyb.vo.UserVO;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/")
 public class UserController {
