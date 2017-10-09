5f4eeacc11514ad8e61ba1c2b6cd9bd7ad25b7c1
hdken <hdken@163.com>
2017-10-09 15:35:11 +0800

    add

---
+++ DA/src/main/java/com/bqhx/yyb/vo/ConstantVO.java
@@ -0,0 +1,38 @@
+package com.bqhx.yyb.vo;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * 常量类
+ *
+ */
+public final class ConstantVO {
+	/**
+	 * dateformat"yyyy-MM-dd HH:mm:ss"
+	 */
+	public static final SimpleDateFormat SDF= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	/**
+	 * 1
+	 */
+	public static final String FLAG_ONE = "1";
+	/**
+	 * 0
+	 */
+	public static final String FLAG_ZERO = "0";
+	/**
+	 * success
+	 */
+	public static final String SUCCESS = "success";
+	/**
+	 * failed
+	 */
+	public static final String FAILED = "failed";
+	/**
+	 * exceltemplate
+	 */
+	public static final String EXCELTEMPLATE = "information-template.xls";
+	/**
+	 * summary table's title
+	 */
+	public static final String SUMMARYTABLETITLE = "业绩日报总表";
+}
