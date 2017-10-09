ca620f899de049588aba9271146d38c6974ef3e8
hdken <hdken@163.com>
2017-10-09 15:18:16 +0800

    s

--- DA/src/main/resources/static/js/ajax.js
+++ DA/src/main/resources/static/js/ajax.js
@@ -1,4 +1,4 @@
-var csUrl = ''; //http://127.0.0.1:8080/login?userId=1&password=1
+var csUrl = ''; //http://127.0.0.1:8080/login?userId=1&password=1   http://192.168.1.129:8080
 var ajaxPost = function(url,data,success,faild){
 	$.ajax({
 		url:url,
