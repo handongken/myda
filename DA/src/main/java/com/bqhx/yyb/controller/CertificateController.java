78c6ecbe86a788d985d65eebff5c4dfdb5c3a313
hdken <hdken@163.com>
2017-11-21 11:35:27 +0800

    CertificateController update 1121

--- DA/src/main/java/com/bqhx/yyb/controller/CertificateController.java
+++ DA/src/main/java/com/bqhx/yyb/controller/CertificateController.java
@@ -50,8 +50,12 @@ public class CertificateController {
 		for(int i = 0;i < certificateVOList.size();i++){
 			CertificateVO certificateVO = certificateVOList.get(i);
 			String str = certificateVO.getInCardNo();
-			String inCardNo = str.substring(str.length() - 4);//银行卡后四位
-			certificateVO.setInCardNo(inCardNo);
+			if(str.length() > 4){
+				String inCardNo = str.substring(str.length() - 4);//银行卡后四位
+				certificateVO.setInCardNo(inCardNo);
+			}else{
+				certificateVO.setInCardNo(str);
+			}
 		}
 		return certificateVOList;
 	}
