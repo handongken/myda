7f3cc9d3bc89f83938076f906fdd67108d13c2ee
hdken <hdken@163.com>
2017-11-23 17:10:50 +0800

    1123 update application

--- DA/src/main/java/com/bqhx/yyb/DaApplication.java
+++ DA/src/main/java/com/bqhx/yyb/DaApplication.java
@@ -1,10 +1,13 @@
 package com.bqhx.yyb;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableAutoConfiguration  
+@EnableAutoConfiguration
+//@EnableEurekaServer
 public class DaApplication {
 
 	public static void main(String[] args) {
