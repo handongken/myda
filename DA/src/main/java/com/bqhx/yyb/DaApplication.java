899fdba38253045b0b07da26a0d1876b9a50f752
hdken <hdken@163.com>
2017-11-23 11:49:54 +0800

    cloud

--- DA/src/main/java/com/bqhx/yyb/DaApplication.java
+++ DA/src/main/java/com/bqhx/yyb/DaApplication.java
@@ -3,11 +3,13 @@ package com.bqhx.yyb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableAutoConfiguration
-//@EnableEurekaServer
 public class DaApplication {
 
 	public static void main(String[] args) {
